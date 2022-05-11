d923ec513825796fefa3f9c7ab4ec83fa39003f8
justine.lim <justine.lim@yiducloud.cn>
2022-05-11 21:33:15 +0800

    Create generate_synthetic_data.sh

---
+++ generate_synthetic_data.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sed -i 's/exporter.csv.export = false/exporter.csv.export = true/' synthea.properties
+
+./run_synthea -c synthea.properties -p 10 -s 0 -cs 0 -r 20220511
+
+find ./output -name "*.csv" -size +50000000c -print | while read file; do
+  fn=$(basename -s .csv ${file})
+  dn=$(dirname ${file})
+  split --additional-suffix=.csv --elide-empty-files --line-bytes=50000000 --numeric-suffixes --suffix-length=2 $file ${dn}/${fn}. && rm $file
+done
